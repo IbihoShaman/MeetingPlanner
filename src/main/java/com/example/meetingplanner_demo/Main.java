0c9a5e16c35928b0dcb36b4bd3332232274bdb3a
IbihoShaman <00zeroone11@gmail.com>
2022-09-08 13:38:48 +0200

    Added config file + minor refractoring

--- src/main/java/com/example/meetingplanner_demo/Main.java
+++ src/main/java/com/example/meetingplanner_demo/Main.java
@@ -1,5 +1,7 @@
 package com.example.meetingplanner_demo;
 
+import java.io.FileReader;
+import java.util.Properties;
 import org.apache.logging.log4j.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,7 +14,7 @@ public class Main extends Application {
     public static Logger logger = LogManager.getLogger(Main.class.getName());
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("View1.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("View/View1.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Meeting Planner 3000©");
         stage.setResizable(false);
@@ -22,6 +24,14 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        try (FileReader reader = new FileReader("config")){
+            Properties properties = new Properties();
+            properties.load(reader);
+            //String string = properties.getProperty("variable");
+            //System.out.println(string);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
         launch();
     }
 }
